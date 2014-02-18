3e7ae3befba2f477b60613d66252c883b2625d44
ilyautkin <ilyautkin@mail.ru>
2014-02-18 21:31:16 +0300

    Update transport.settings.php

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -3,11 +3,11 @@
 $settings = array();
 
 $tmp = array(
-	'some_setting' => array(
+	/*'some_setting' => array(
 		'xtype' => 'combo-boolean',
 		'value' => true,
 		'area' => 'autoredirector_main',
-	),
+	),*/
 );
 
 foreach ($tmp as $k => $v) {
