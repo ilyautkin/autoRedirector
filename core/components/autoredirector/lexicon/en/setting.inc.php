bf8aee46ac20762ecfc52d2001ebc9c9ea4cb3b1
Alex <legi.ero@gmail.com>
2017-02-02 13:22:03 +0300

    Update setting.inc.php
    Update english setting

--- core/components/autoredirector/lexicon/en/setting.inc.php
+++ core/components/autoredirector/lexicon/en/setting.inc.php
@@ -6,4 +6,4 @@ $_lang['setting_autoredirector_some_setting'] = 'Some setting';
 $_lang['setting_autoredirector_some_setting_desc'] = 'This is description for some setting';
 
 $_lang['setting_autoredirector_clear_get'] = 'Clear GET-parameters';
-$_lang['setting_autoredirector_some_setting_desc'] = 'Clear all GET-parameters on redirect';
+$_lang['setting_autoredirector_clear_get_desc'] = 'Clear all GET-parameters on redirect';
